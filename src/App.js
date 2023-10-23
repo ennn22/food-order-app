c3d92f2ff8fb3cd99c17ba82b63cd318bf85609c
en <huien122@gmail.com>
2023-10-23 21:22:51 +0800

    First commit.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import ItemsContext from './Store/ItemsContext';
+import { Fragment } from 'react';
+
 
 function App() {
+  const itemsCtx = useContext(ItemsContext);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      {itemsCtx.switchPage ? <Users /> : <Admin />}
+      <Footer />
+    </Fragment>
   );
 }
 
---
+++ src/Store/ItemsContext
@@ -0,0 +1,12 @@
+import { createContext } from "react";
+
+const ItemsContext = createContext({
+  itemsData: [],
+  switchPage: null,
+  addNewItem: (item) => {},
+  removeItem: (id) => {},
+  updateItem: (id) => {},
+  togglePage: () => {},
+});
+
+export default ItemsContext;
